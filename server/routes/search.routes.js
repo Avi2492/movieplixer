eb72ba2e8db75631b121ca4dbb1aba50800f7b4f
Avinash Pandey <avinash.12009084@lpu.in>
2024-07-22 00:07:10 +0530

    Search API's

---
+++ server/routes/search.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getSearchHistory,
+  removeItemFromSearchHistory,
+  searchMovie,
+  searchPerson,
+  searchTvShow,
+} from "../controllers/search.controller.js";
+
+const router = express.Router();
+
+router.get("/person/:query", searchPerson);
+router.get("/movie/:query", searchMovie);
+router.get("/tv/:query", searchTvShow);
+router.get("/history", getSearchHistory);
+router.delete("/history/:id", removeItemFromSearchHistory);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
+import cookieParser from "cookie-parser";
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
+import tvRoutes from "./routes/tv.routes.js";
+import searchRoutes from "./routes/search.routes.js";
+// import wishlistRoutes from "./routes/wishlist.routes.js";
 
 import { protectRoute } from "./middlewares/protectRoute.js";
-import tvRoutes from "./routes/tv.routes.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectToDb } from "./db/connectToDb.js";
-import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -17,6 +19,8 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
+// app.use("/api/v1/wishlist", protectRoute, wishlistRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Movieplix API is Running" });

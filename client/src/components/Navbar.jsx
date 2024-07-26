487df712c0afd8fc414357a8c12e17810d2f6936
Avinash Pandey <avinash.12009084@lpu.in>
2024-07-26 14:11:10 +0530

    Home Screen

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { RiLogoutBoxRLine, RiMenuLine, RiSearchLine } from "@remixicon/react";
+import { useAuthStore } from "../store/authUser.js";
+import Logo from "./Logo";
+import { useContentStore } from "../store/content.js";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { user, logout } = useAuthStore();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const { setContentType } = useContentStore();
+
+  return (
+    <>
+      <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
+        <div className="flex items-center gap-10 z-50">
+          <Link to={"/"}>
+            <Logo />
+          </Link>
+          <div className="hidden sm:flex gap-2 items-center">
+            <Link
+              to={"/"}
+              className="hover:underline"
+              onClick={() => setContentType("movie")}
+            >
+              Movies
+            </Link>
+            <Link
+              to={"/"}
+              className="hover:underline"
+              onClick={() => setContentType("tv")}
+            >
+              Tv Shows
+            </Link>
+            <Link to={"/"} className="hover:underline">
+              Search History
+            </Link>
+            <Link to={"/"} className="hover:underline">
+              Watch List
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex gap-2 items-center z-50">
+          <Link to={"/"} className="hover:underline">
+            <RiSearchLine size={20} className="cursor-pointer" />
+          </Link>
+          <img
+            src={user.image}
+            alt="avatar"
+            className="h-8 rounded cursor-pointer"
+          />
+          <RiLogoutBoxRLine
+            size={20}
+            onClick={logout}
+            className="cursor-pointer"
+          />
+
+          <div className="sm:hidden">
+            <RiMenuLine
+              size={20}
+              onClick={toggleMenu}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
+
+        {isOpen && (
+          <div className="w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800">
+            <Link to={"/"} className="block p-2 hover:underline">
+              Movies
+            </Link>
+            <Link to={"/"} className="block p-2 hover:underline">
+              Tv Shows
+            </Link>
+            <Link to={"/"} className="block p-2 hover:underline">
+              Search History
+            </Link>
+            <Link to={"/"} className="block p-2 hover:underline">
+              Watch List
+            </Link>
+          </div>
+        )}
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
+
+// testing account -> foromo9066@fuzitea.com 1234567890
--- client/src/pages/home/HomeScreen.jsx
+++ client/src/pages/home/HomeScreen.jsx
@@ -1,17 +1,58 @@
 import React from "react";
-import { useAuthStore } from "../../store/authUser.js";
+
+import Navbar from "../../components/Navbar.jsx";
+import { Link } from "react-router-dom";
+import { RiInformationLine, RiPlayLine } from "@remixicon/react";
+import useGetTrendingContent from "../../hooks/useGetTrendingContent.jsx";
 
 const HomeScreen = () => {
-  const { logout } = useAuthStore();
+  const { trendingContent } = useGetTrendingContent();
   return (
-    <div>
-      <button
-        onClick={logout}
-        className="bg-orange-500 rounded-lg text-white p-2"
-      >
-        Logout
-      </button>
-    </div>
+    <>
+      <div className="h-screen relative text-white">
+        <Navbar />
+
+        <img
+          src="/assets/extraction.jpg"
+          alt="hero-img"
+          className="absolute top-0 left-0 w-full h-full object-cover -z-50"
+        />
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
+          aria-hidden="true"
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
+          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
+
+          <div className="max-w-2xl">
+            <h1 className="mt-4 text-6xl font-extrabold text-balance">
+              Extraction
+            </h1>
+
+            <p className="mt-2 text-lg">2014 | 18+</p>
+
+            <p className="mt-4 text-lg">
+              A former DEA agent moves his family to a quiet town, where he soon
+            </p>
+          </div>
+
+          <div className="flex mt-8">
+            <Link
+              to={"/watch/123"}
+              className="bg-white hover:bg-white/80 font-bold py-2 px-4 rounded mr-4 flex items-center text-black"
+            >
+              <RiPlayLine size={20} className="mr-2 text-black" /> Play
+            </Link>
+            <Link
+              to={"/watch/123"}
+              className="bg-orange-500 hover:bg-orange-600 font-bold py-2 px-4 rounded mr-4 flex items-center text-white"
+            >
+              <RiInformationLine size={20} className="mr-2" /> Info
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 

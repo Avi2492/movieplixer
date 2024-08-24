592d2054ec0d53e87a8012782ddeb27a092f4278
Avinash Pandey <avinash.12009084@lpu.in>
2024-08-25 00:11:37 +0530

    Search Page hover

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -36,16 +36,16 @@ const SearchPage = () => {
   };
 
   return (
-    <div className="bg-black min-h-screen text-orange-500">
+    <div className="bg-black min-h-screen text-white">
       <Navbar />
       <div className="container mx-auto px-4 py-8">
         <div className="flex justify-center gap-3 mb-4">
           <button
             className={`py-2 px-4 rounded ${
-              active === "tv"
+              active === "movie"
                 ? "bg-orange-500 text-white"
-                : "bg-gray-100 text-orange-500"
-            } hover:bg-orange-600 text-white`}
+                : "bg-gray-100 text-orange-600"
+            } hover:bg-orange-600 hover:text-white font-semibold`}
             onClick={() => handleTabClick("movie")}
           >
             Movies
@@ -54,18 +54,18 @@ const SearchPage = () => {
             className={`py-2 px-4 rounded ${
               active === "tv"
                 ? "bg-orange-500 text-white"
-                : "bg-gray-100 text-orange-500"
-            } hover:bg-orange-600 text-white`}
+                : "bg-gray-100 text-orange-600"
+            } hover:bg-orange-600 hover:text-white font-semibold`}
             onClick={() => handleTabClick("tv")}
           >
             TV Shows
           </button>
           <button
             className={`py-2 px-4 rounded ${
-              active === "tv"
+              active === "person"
                 ? "bg-orange-500 text-white"
-                : "bg-gray-100 text-orange-500"
-            } hover:bg-orange-600 text-white`}
+                : "bg-gray-100 text-orange-600"
+            } hover:bg-orange-600 hover:text-white font-semibold`}
             onClick={() => handleTabClick("person")}
           >
             Person
